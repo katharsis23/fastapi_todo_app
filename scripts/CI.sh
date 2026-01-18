172d9f5bab593c838771665d73194248da0434a8
katharsis23 <daniklv2006@gmail.com>
2026-01-18 21:10:53 +0200

    Added CI.sh

--- scripts/CI.sh
+++ scripts/CI.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# Визначення кольорів
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color (скидання)
+NC='\033[0m' # No Color
 
-# Функції для гарного виводу
+
 info() { echo -e "${BLUE}[INFO]${NC} $1"; }
 success() { echo -e "${GREEN}[SUCCESS]${NC} ✅ $1"; }
 warning() { echo -e "${YELLOW}[WARNING]${NC} ⚠️  $1"; }
